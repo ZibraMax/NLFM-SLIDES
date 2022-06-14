c20537eb34c8771c14b0beb44a6e9e07a3e19671
Arturo <da.rodriguezh@uniandes.edu.co>
2022-06-14 17:57:21 -0500

    Cmaps working!

--- python/main.py
+++ python/main.py
@@ -7,6 +7,16 @@ def ate(p):
     return np.exp(-p)
 
 
+def ate2(p):
+    y = (1-p/(1+4))
+    return y*(y > 0)
+
+
+def ate3(p):
+    y = (1-p**2/(1+4)**2)
+    return y*(y > 0)
+
+
 x = np.linspace(-5, 5, 50)
 y = np.linspace(-5, 5, 50)
 xx, yy = np.meshgrid(x, y)
@@ -15,6 +25,18 @@ y = yy.flatten()
 p = (x**2+y**2)**0.5
 z = ate(p)
 np.savetxt('./resources/atenuacion_1.csv',
+           [x, y, z], delimiter=',', fmt='%.4f')
+plt.figure().add_subplot(projection='3d').plot_trisurf(x, y, z, cmap='rainbow')
+plt.show()
+
+z = ate2(p)
+np.savetxt('./resources/atenuacion_2.csv',
+           [x, y, z], delimiter=',', fmt='%.4f')
+plt.figure().add_subplot(projection='3d').plot_trisurf(x, y, z, cmap='rainbow')
+plt.show()
+
+z = ate3(p)
+np.savetxt('./resources/atenuacion_3.csv',
            [x, y, z], delimiter=',', fmt='%.4f')
 plt.figure().add_subplot(projection='3d').plot_trisurf(x, y, z, cmap='rainbow')
 plt.show()
