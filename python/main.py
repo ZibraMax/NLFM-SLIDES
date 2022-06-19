6c08e6d999f91eb11dac19036723ae9fe7acd166
Arturo Rodriguez <davidarturo9911@hotmail.com>
2022-06-18 20:27:59 -0500

    Menos puntos

--- python/main.py
+++ python/main.py
@@ -7,9 +7,6 @@ def ate(p):
     return np.exp(-p)
 
 
-def atei(x): return -np.log(x)
-
-
 def ate2(p):
     y = (1-p/(1+4))
     return y*(y > 0)
@@ -20,31 +17,27 @@ def ate3(p):
     return y*(y > 0)
 
 
-maxr = 5
-n = 5
-x1 = atei(np.linspace(ate(0), ate(maxr), n)).tolist()
-x2 = (-atei(np.linspace(ate(maxr), ate(0), n))).tolist()
-
-_x = np.array(x2+x1[1:])
-xx, yy = np.meshgrid(_x, _x)
+x = np.linspace(-5, 5, 21)
+y = np.linspace(-5, 5, 21)
+xx, yy = np.meshgrid(x, y)
 x = xx.flatten()
 y = yy.flatten()
 p = (x**2+y**2)**0.5
 z = ate(p)
-print(len(z))
 np.savetxt('./resources/atenuacion_1.csv',
-           [x, y, z], delimiter=',', fmt='%.4f')
+           [x, y, z], delimiter=',', fmt='%.5f')
+
 plt.figure().add_subplot(projection='3d').plot_trisurf(x, y, z, cmap='rainbow')
 plt.show()
 
 z = ate2(p)
 np.savetxt('./resources/atenuacion_2.csv',
-           [x, y, z], delimiter=',', fmt='%.4f')
+           [x, y, z], delimiter=',', fmt='%.5f')
 plt.figure().add_subplot(projection='3d').plot_trisurf(x, y, z, cmap='rainbow')
 plt.show()
 
 z = ate3(p)
 np.savetxt('./resources/atenuacion_3.csv',
-           [x, y, z], delimiter=',', fmt='%.4f')
+           [x, y, z], delimiter=',', fmt='%.5f')
 plt.figure().add_subplot(projection='3d').plot_trisurf(x, y, z, cmap='rainbow')
 plt.show()
